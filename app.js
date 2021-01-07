094ef830be970bd883051784883de068c312e7d1
babyazalea <usun12@hotmail.com>
2021-01-08 00:52:28 +0900

    using Event Modifier

--- app.js
+++ app.js
@@ -2,9 +2,20 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
+      confirmedName: "",
     };
   },
   methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm() {
+      alert("Submitted!");
+    },
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
+    },
     add(num) {
       this.counter = this.counter + num;
     },
