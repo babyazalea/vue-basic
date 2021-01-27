21920f24762fff9eda1036cfac2029e1838702f6
babyazalea <usun12@hotmail.com>
2021-01-27 14:48:35 +0900

    using index in v-for

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const app = Vue.createApp({
   data() {
-    return { enteredGoalValue: "", goals: [] };
+    return {
+      enteredGoalValue: "",
+      goals: [],
+    };
   },
   methods: {
     addGoal() {
       this.goals.push(this.enteredGoalValue);
+    },
+    removeGoal(idx) {
+      this.goals.splice(idx, 1);
     },
   },
 });
